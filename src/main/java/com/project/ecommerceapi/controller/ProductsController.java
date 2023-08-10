7baf9c4a113dd1a58d0b86b65a8938bf538c12d4
EtiamSani <etiam.sani@laposte.net>
2023-08-10 19:16:38 +0200

    Add logger

--- src/main/java/com/project/ecommerceapi/controller/ProductsController.java
+++ src/main/java/com/project/ecommerceapi/controller/ProductsController.java
@@ -2,6 +2,8 @@ package com.project.ecommerceapi.controller;
 
 import com.project.ecommerceapi.entity.ProductsEntity;
 import com.project.ecommerceapi.service.ProductsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,17 +12,20 @@ import java.util.List;
 @RequestMapping("/api/v1/products")
 public class ProductsController {
     private final ProductsService productsService;
+    private static final Logger logger = LoggerFactory.getLogger(ProductsController.class);
 
     public ProductsController(ProductsService productsService) {
         this.productsService = productsService;
     }
     @GetMapping
     public List<ProductsEntity> findAllProducts(){
+        logger.info("Fetching all products");
         return productsService.findAllProducts();
     }
 
     @PostMapping
     public ProductsEntity saveProducts(@RequestBody ProductsEntity productsEntity){
+        logger.info("Saving product: {}", productsEntity);
         return productsService.saveProducts(productsEntity);
     }
 }
