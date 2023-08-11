18b4196fc4219d6690f3619859739049d1589dcc
EtiamSani <etiam.sani@laposte.net>
2023-08-11 16:40:34 +0200

    Tests

--- src/test/java/com/project/ecommerceapi/ECommerceApiApplicationTests.java
+++ src/test/java/com/project/ecommerceapi/ECommerceApiApplicationTests.java
@@ -1,13 +1,38 @@
 package com.project.ecommerceapi;
 
+import com.project.ecommerceapi.controller.ProductsController;
+import com.project.ecommerceapi.service.ProductsService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-class ECommerceApiApplicationTests {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.*;
+
+@WebMvcTest(ProductsController.class)
+public class ProductsControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private ProductsService productsService;
 
 	@Test
-	void contextLoads() {
+	public void testGetAllProducts() throws Exception {
+		// Mock the behavior of the service
+		when(productsService.findAllProducts()).thenReturn();
+
+		// Perform the request and validate the response
+		mockMvc.perform(get("/api/products"))
+				.andExpect(status().isOk())
+				.andExpect(/* Add more assertions based on your expected response */);
 	}
 
+	// Add more test methods for other controller methods
+
 }
+
